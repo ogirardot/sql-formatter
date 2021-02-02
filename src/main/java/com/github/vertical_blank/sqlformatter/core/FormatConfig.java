3fa0102cb91fd206c9a7bc20d2845409a5b8ceb3
yohei.yamana <fixeight@gmail.com>
2021-02-03 01:01:36 +0900

    add config.uppercase

--- src/main/java/com/github/vertical_blank/sqlformatter/core/FormatConfig.java
+++ src/main/java/com/github/vertical_blank/sqlformatter/core/FormatConfig.java
@@ -6,10 +6,12 @@ public class FormatConfig {
 
 	public final String indent;
 	public final Params.Holder params;
+	public final boolean uppercase;
 
-	FormatConfig(String indent, Params.Holder params) {
+	FormatConfig(String indent, Params.Holder params, boolean uppercase) {
 		this.indent = indent;
 		this.params = params;
+		this.uppercase = uppercase;
 	}
 
 	public static FormatConfigBuilder builder() {
@@ -19,22 +21,37 @@ public class FormatConfig {
 	public static class FormatConfigBuilder {
 		private String indent;
 		private Params.Holder params;
+		private boolean uppercase;
 
 		FormatConfigBuilder() {
 		}
 
+		/**
+		 * @param indent Characters used for indentation, default is "  " (2 spaces)
+		 */
 		public FormatConfigBuilder indent(String indent) {
 			this.indent = indent;
 			return this;
 		}
 
+		/**
+		 * @param params Collection of params for placeholder replacement
+		 */
 		public FormatConfigBuilder params(Params.Holder params) {
 			this.params = params;
 			return this;
 		}
 
+		/**
+		 * @param uppercase Converts keywords to uppercase
+		 */
+		public FormatConfigBuilder uppercase(boolean uppercase) {
+			this.uppercase = uppercase;
+			return this;
+		}
+
 		public FormatConfig build() {
-			return new FormatConfig(indent, params);
+			return new FormatConfig(this.indent, this.params, this.uppercase);
 		}
 	}
 }
